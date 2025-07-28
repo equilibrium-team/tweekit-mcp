88f85e59df0168fc15655c948215985f84f12605
dpeq <dpochron@equilibrium.com>
2025-07-28 16:09:41 -0500

    Initial commit

---
+++ server.py
@@ -0,0 +1,59 @@
+import asyncio
+import httpx
+import logging
+import os
+
+from fastmcp import FastMCP 
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(format="[%(levelname)s]: %(message)s", level=logging.INFO)
+
+mcp = FastMCP("TweekIT MCP Server on Cloud Run")
+
+@mcp.tool()
+async def version() -> str:
+    """Get the version of the MCP server from the remote API."""
+    url = "https://dapp.tweekit.io/tweekit/api/image/version"
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url)
+        response.raise_for_status()
+        return response.text
+
+@mcp.tool()
+def add(a: int, b: int) -> int:
+    """Use this to add two numbers together.
+
+    Args:
+        a: The first number.
+        b: The second number.
+
+    Returns:
+        The sum of the two numbers.
+    """
+    logger.info(f">>> 🛠️ Tool: 'add' called with numbers '{a}' and '{b}'")
+    return a + b
+
+@mcp.tool()
+def subtract(a: int, b: int) -> int:
+    """Use this to subtract two numbers.
+
+    Args:
+        a: The first number.
+        b: The second number.
+
+    Returns:
+        The difference of the two numbers.
+    """
+    logger.info(f">>> 🛠️ Tool: 'subtract' called with numbers '{a}' and '{b}'")
+    return a - b
+
+if __name__ == "__main__":
+    logger.info(f"🚀 MCP server started on port {os.getenv('PORT', 8080)}")
+    # Could also use 'sse' transport, host="0.0.0.0" required for Cloud Run.
+    asyncio.run(
+        mcp.run_async(
+            transport="streamable-http",
+            host="0.0.0.0",
+            port=os.getenv("PORT", 8080),
+        )
+    )
