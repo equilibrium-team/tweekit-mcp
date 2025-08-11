fc85c6d507f027df380589614012615092f5f002
dpeq <dpochron@equilibrium.com>
2025-08-11 13:15:25 -0500

    Added support for converting Office docs to multipage PDFs using this new feature in TweekIT. Upgraded FastMCP to v2 to handle return of PDFs. Updated docs.

--- server.py
+++ server.py
@@ -4,17 +4,8 @@ import logging
 import os
 
 from fastmcp import FastMCP
-from fastmcp.utilities.types import Image
-from mcp.types import ImageContent
+from fastmcp.utilities.types import Image, File
 from typing import Any, Dict
-
-# localhost
-#/5reAwjAniUsgczI/ggoVxqQYPoKwzsV8nVK8jJD
-#Ri8/Htd9igtWiS/yKHFgeBHFxuhEMOhXEns9lvtd
-
-# tweekit.io API key and secret
-#+eyzMhg3XMP8ErS9L1hfUwUTQN9fh/nTtZOrajZD
-#SltST77UAWPyoIbXgqD6weQDigdcJbOiDS7414uK
 
 BASE_URL = "https://dapp.tweekit.io/tweekit/api/image/"
 #BASE_URL = "http://localhost:16377/api/image/"
@@ -24,7 +15,7 @@ logging.basicConfig(format="[%(levelname)s]: %(message)s", level=logging.WARNING
 
 mcp = FastMCP("TweekIT MCP Server - normalize almost any file for AI ingestion")
 
-@mcp.tool()
+@mcp.resource("config://tweekit-version")
 async def version() -> str:
     """Get current version of the TweekIT API."""
     url = f"{BASE_URL}version"
@@ -34,14 +25,14 @@ async def version() -> str:
         return response.text
 
 @mcp.tool()
-async def doctype(ext: str, apiKey: str, apiSecret: str) -> Dict[str, Any]:
+async def doctype(apiKey: str, apiSecret: str, extension: str = "*") -> Dict[str, Any]:
     """
     Retrieve a list of supported file formats or map a file extension to its document type.
 
     Args:
-        ext (str): The file extension to query (e.g., 'jpg', 'pdf').
         apiKey (str): The API key for authentication.
         apiSecret (str): The API secret for authentication.
+        extension (str): The file extension to query (e.g., 'jpg', 'pdf') or leave off to return all supported input formats.
 
     Returns:
         Dict[str, Any]: A dictionary containing the supported file formats or an error message.
@@ -49,19 +40,20 @@ async def doctype(ext: str, apiKey: str, apiSecret: str) -> Dict[str, Any]:
     url = f"{BASE_URL}doctype"
     async with httpx.AsyncClient(timeout=10.0) as client:
         try:
-            client.headers["ApiKey"] = apiKey
-            client.headers["ApiSecret"] = apiSecret
-            response = await client.get(url)
+            response = await client.get(url, headers={"ApiKey": apiKey, "ApiSecret": apiSecret}, params={"extension": extension})
             response.raise_for_status()  # Raise an exception for HTTP errors
 
-            # FastMCP will automatically serialize this dict to JSON and set the mime_type
-            return response.json()
+            data = response.json()
+            if isinstance(data, dict):
+                return data
+            else:
+                return {"result": data}
 
         except httpx.HTTPStatusError as e:
-            print(f"HTTP error fetching supported file formats given '{ext}': {e}")
+            print(f"HTTP error fetching supported file formats given '{extension}': {e}")
             return {"error": f"Failed to fetch user data. Status: {e.response.status_code}"}
         except httpx.RequestError as e:
-            print(f"Network error fetching supported file formats given '{ext}': {e}")
+            print(f"Network error fetching supported file formats given '{extension}': {e}")
             return {"error": f"Network error: {e}"}
         except Exception as e:
             print(f"An unexpected error occurred: {e}")
@@ -74,6 +66,7 @@ async def convert(
     inext: str,
     outfmt: str,
     blob: str,
+    noRasterize: bool = False,
     width: int = 0,
     height: int = 0,
     page: int = 1,
@@ -86,8 +79,9 @@ async def convert(
         apiKey (str): The API key for authentication.
         apiSecret (str): The API secret for authentication.
         inext (str): The input file extension (e.g., 'jpg', 'png').
-        outfmt (str): The desired output format (e.g., 'pdf', 'png').
+        outfmt (str): The desired output format. Current supported types include 'jpg', 'png', 'webp', 'bmp', and 'pdf'.
         blob (str): The base64-encoded document data.
+        noRasterize (bool, optional): For input document formats that are not raster images, do not rasterize the document - return it as a PDF. (outfmt must be set to pdf). The parameters listed below are ignored when doing document to PDF conversions.
         width (int, optional): The desired width of the output. Defaults to 0 (no resizing).
         height (int, optional): The desired height of the output. Defaults to 0 (no resizing).
         page (int, optional): The page number to convert (for multi-page documents). Defaults to 1.
@@ -107,16 +101,19 @@ async def convert(
                 "Height": height,
                 "BgColor": bgcolor,
                 "Page": page,
+                "NoRasterize": noRasterize,
                 "DocDataType": inext,
                 "DocData": blob
             })
             response.raise_for_status()  # Raise an exception for HTTP errors
 
             content_type = response.headers.get("content-type")
-            if content_type and "image/" in content_type:
-                image_format = content_type.split("/")[-1]
-                img_obj = Image(data=response.content, format=image_format)
-                return img_obj.to_image_content()
+            if content_type:
+                if "image/" in content_type:
+                    image_format = content_type.split("/")[-1]
+                    return Image(data=response.content, format=image_format)
+                elif content_type == "application/pdf":
+                    return File(data=response.content, format="pdf")
 
             # Default case if content type is unknown
             return {"error": f"Unsupported content type in response: '{content_type}'"}
@@ -131,33 +128,6 @@ async def convert(
             print(f"An unexpected error occurred: {e}")
             return {"error": f"An unexpected error occurred: {e}"}
 
-# @mcp.tool()
-# def add(a: int, b: int) -> int:
-#     """Use this to add two numbers together.
-
-#     Args:
-#         a: The first number.
-#         b: The second number.
-
-#     Returns:
-#         The sum of the two numbers.
-#     """
-#     logger.info(f">>> 🛠️ Tool: 'add' called with numbers '{a}' and '{b}'")
-#     return a + b
-
-# @mcp.tool()
-# def subtract(a: int, b: int) -> int:
-#     """Use this to subtract two numbers.
-
-#     Args:
-#         a: The first number.
-#         b: The second number.
-
-#     Returns:
-#         The difference of the two numbers.
-#     """
-#     logger.info(f">>> 🛠️ Tool: 'subtract' called with numbers '{a}' and '{b}'")
-#     return a - b
 
 if __name__ == "__main__":
     logger.info(f"🚀 TweekIT MCP server started on port {os.getenv('PORT', 8080)}")
